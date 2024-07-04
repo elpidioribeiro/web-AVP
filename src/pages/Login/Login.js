a5b5cc3545b2b9ffc7c8aded339cf6a89c3eec08
Elpidio Ribeiro de Neto <elpidio.neto@adtsa.local>
2024-07-04 11:50:34 -0300

    Segundo Commit

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,26 +2,48 @@ import logo from '../../assets/auto_viao_progresso_logo-removebg-preview.png'
 import './Login.css'
 import imgUser from '../../assets/user.png'
 import imgPassword from '../../assets/lock.png'
+import { useState } from 'react';
+
 
 
 
 
 function Login () {
+
+    const [form, setForm] = useState({
+        email: '',
+        senha: ''
+    });
+
+    function handleChangeInput(e) {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
+    function handleChangeSubmit(e) {
+        e.preventDefault();
+        console.log(form)
+        const { email, senha } = form;
+        if (!email || !senha) {
+         alert('Preencha email e senha');
+            return;
+        }
+    }
+
     return (
         <>
         <div className='container-main'>
-        <div className='container-login'>
+        <form className='container-login'onSubmit={handleChangeSubmit}>
          <img className = 'logo' src= {logo} />
         <div className='login'> 
          <img className = 'usuario' src= {imgUser} />
-         <input placeholder='USUÁRIO' type='text'/>
+         <input placeholder='USUÁRIO' name='email' type='text' onChange={(e) => { handleChangeInput(e) }}/>
          </div>
          <div className='senha'> 
           <img className = 'usuario' src= {imgPassword} />
-          <input placeholder='SENHA'/>
+          <input placeholder='SENHA' name='senha' onChange={(e) => { handleChangeInput(e) }}/>
          </div>
          <button className='botao' > ENTRAR </button>   
-        </div>
+        </form>
         </div>
         </>
         
