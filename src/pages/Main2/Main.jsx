784b4d4fad7e8eb079f3632e1fbca09f44fbac49
Elpidio Ribeiro de Neto <elpidio.neto@adtsa.local>
2024-10-10 13:57:39 -0300

    teste

--- src/pages/Main2/Main.jsx
+++ src/pages/Main2/Main.jsx
@@ -7,8 +7,8 @@ import { useState } from "react";
 function Main () {
     
     const [statePage, setStatePage] = useState(-1);
-    const src1 = "https://app.powerbi.com/view?r=eyJrIjoiMTI2ZTc3YzYtMTU3Mi00MGZmLTlhMzktMjg2ZWVkZGEwYzExIiwidCI6ImQ4ZDVjZTk1LWJjM2UtNDNkMS1iOWJhLTgzNDdlODdhOGE1YyJ9"
-    const src2 = "https://app.powerbi.com/view?r=eyJrIjoiMzRhYzMxNzgtYzhiMi00OGViLTlkMzctMjMxZDI2N2RhNmFjIiwidCI6ImQ4ZDVjZTk1LWJjM2UtNDNkMS1iOWJhLTgzNDdlODdhOGE1YyJ9"
+    // const src1 = "https://app.powerbi.com/view?r=eyJrIjoiMTI2ZTc3YzYtMTU3Mi00MGZmLTlhMzktMjg2ZWVkZGEwYzExIiwidCI6ImQ4ZDVjZTk1LWJjM2UtNDNkMS1iOWJhLTgzNDdlODdhOGE1YyJ9"
+    // const src2 = "https://app.powerbi.com/view?r=eyJrIjoiMzRhYzMxNzgtYzhiMi00OGViLTlkMzctMjMxZDI2N2RhNmFjIiwidCI6ImQ4ZDVjZTk1LWJjM2UtNDNkMS1iOWJhLTgzNDdlODdhOGE1YyJ9"
     
     function handleChangePage() {
         let statePageAtual = statePage*(-1)
@@ -20,8 +20,8 @@ function Main () {
     return (<div className="Main"> 
         <Sidebar/>
         <div className = "powerBi">
-        <img alt = "" className={"logoHistorico"} src= {statePage === 1? biavp : historico} onClick={() => handleChangePage()} />    
-        <iframe className= "BI" title="PAINEL GERENCIAL2" width="800" height="373.5" src={statePage === 1 ? src2 : src1} frameborder="0" allowFullScreen="true"></iframe>
+        {/* <img alt = "" className={"logoHistorico"} src= {statePage === 1? biavp : historico} onClick={() => handleChangePage()} />    
+        <iframe className= "BI" title="PAINEL GERENCIAL2" width="800" height="373.5" src={statePage === 1 ? src2 : src1} frameborder="0" allowFullScreen="true"></iframe> */}
         </div>
         </div>)
 }
